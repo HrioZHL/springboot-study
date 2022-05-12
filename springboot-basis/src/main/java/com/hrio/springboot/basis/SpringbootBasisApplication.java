385aa461073a7637f713cdee0001417feac72fb8
Hrio <1085568138@qq.com>
2022-05-12 16:46:50 +0800

    feat：新增springboot基础模块，helloworld相关代码

--- springboot-basis/src/main/java/com/hrio/springboot/basis/SpringbootBasisApplication.java
+++ springboot-basis/src/main/java/com/hrio/springboot/basis/SpringbootBasisApplication.java
@@ -3,6 +3,12 @@ package com.hrio.springboot.basis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Springboot主启动类
+ *
+ * @author hrio
+ * @date 2022/5/12
+ */
 @SpringBootApplication
 public class SpringbootBasisApplication {
 
---
+++ springboot-basis/src/main/java/com/hrio/springboot/basis/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.hrio.springboot.basis.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试Controller
+ *
+ * @author zhanghl
+ * @date 2022/05/12
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!!!";
+    }
+}
