56f4542e119cf00b943712f3e2e61821b18262d0
Hrio <1085568138@qq.com>
2022-05-12 10:44:48 +0800

    Initial commit

---
+++ springboot-basis/src/main/java/com/hrio/springboot/basis/SpringbootBasisApplication.java
@@ -0,0 +1,13 @@
+package com.hrio.springboot.basis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBasisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBasisApplication.class, args);
+    }
+
+}
---
+++ springboot-basis/src/test/java/com/hrio/springboot/basis/SpringbootBasisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrio.springboot.basis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBasisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
